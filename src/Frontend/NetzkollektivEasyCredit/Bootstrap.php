f990e87b27edd9616297c2ddf6cf6e9d940ba555
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2016-12-09 12:51:29 +0100

    version bump to 1.2.5

--- src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
+++ src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
@@ -18,7 +18,7 @@ class Shopware_Plugins_Frontend_NetzkollektivEasyCredit_Bootstrap
 
     public function getVersion()
     {
-        return '1.2.4';
+        return '1.2.5';
     }
 
     public function getInfo()
