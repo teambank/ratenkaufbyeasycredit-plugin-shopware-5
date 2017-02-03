35c2260572469397f369add99946d7b9d7f71da7
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2017-02-03 15:13:49 +0100

    Version bump to 1.2.5.1; pp plugin scripts only included on detail page for all shopware versions

--- src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
+++ src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
@@ -18,7 +18,7 @@ class Shopware_Plugins_Frontend_NetzkollektivEasyCredit_Bootstrap
 
     public function getVersion()
     {
-        return '1.2.5';
+        return '1.2.5.1';
     }
 
     public function getInfo()
