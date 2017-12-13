82bb2bc04cb628aaf256177787164a6a977f7f08
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2017-12-13 15:59:54 +0100

    updated api lib, release 1.4.1

--- src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
+++ src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
@@ -19,7 +19,7 @@ class Shopware_Plugins_Frontend_NetzkollektivEasyCredit_Bootstrap
 
     public function getVersion()
     {
-        return '1.4.0';
+        return '1.4.1';
     }
 
     public function getInfo()
