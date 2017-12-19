0bd2445c0367736fd1e9f69692118250a631e4d7
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2017-12-19 11:30:24 +0100

    skip products without price (leading to api error)

--- src/Frontend/NetzkollektivEasyCredit/Api/Quote.php
+++ src/Frontend/NetzkollektivEasyCredit/Api/Quote.php
@@ -78,6 +78,9 @@ class Quote implements \Netzkollektiv\EasyCreditApi\Rest\QuoteInterface {
             $_items[] = new Quote\Item(
                 $item
             );
+            if ($quoteItem->getPrice() == 0) {
+                continue;
+            }
         }
         return $_items;
     }
