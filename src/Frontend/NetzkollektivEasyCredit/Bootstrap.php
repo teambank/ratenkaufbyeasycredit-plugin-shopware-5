1a515e4297d303586461bf50ff158c6aaa8fe876
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2018-01-25 14:50:03 +0100

    version to 1.4.4

--- src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
+++ src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
@@ -19,7 +19,7 @@ class Shopware_Plugins_Frontend_NetzkollektivEasyCredit_Bootstrap
 
     public function getVersion()
     {
-        return '1.4.3';
+        return '1.4.4';
     }
 
     public function getInfo()
