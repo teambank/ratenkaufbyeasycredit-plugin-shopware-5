138381ac394b7e46b7304df7a4ce7b959712f292
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2019-08-26 11:47:46 +0200

    updated widget to work with amounts up to 10.000 EUR

--- src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
+++ src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
@@ -20,7 +20,7 @@ class Shopware_Plugins_Frontend_NetzkollektivEasyCredit_Bootstrap
 
     public function getVersion()
     {
-        return '1.6.1';
+        return '1.6.2';
     }
 
     public function getInfo()
