70661c8c5fe3c56b697036bf4ef8748d9378844d
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2021-07-06 16:47:38 +0200

    version up

--- src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
+++ src/Frontend/NetzkollektivEasyCredit/Bootstrap.php
@@ -24,7 +24,7 @@ class Shopware_Plugins_Frontend_NetzkollektivEasyCredit_Bootstrap
 
     public function getVersion()
     {
-        return '1.8.3';
+        return '1.8.4';
     }
 
     public function getInfo()
